6e5fcc347257fcb5156de5639e993b11cee754d4
RAJKUMAR30082001 <125723189+RAJKUMAR30082001@users.noreply.github.com>
2023-12-04 10:20:19 +0530

    Add files via upload

---
+++ swap.js
@@ -0,0 +1,7 @@
+let first_num=10
+let second_num=20
+let temp=0
+temp=first_num
+first_num=second_num
+second_num=temp
+console.log(" first_number value="+first_num,"\n","second number value="+second_num)
