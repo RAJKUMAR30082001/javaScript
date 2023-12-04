21874f6394bb5096ef8934c6443d91871a774ba4
RAJKUMAR30082001 <125723189+RAJKUMAR30082001@users.noreply.github.com>
2023-12-04 21:41:49 +0530

    Add files via upload

---
+++ caps.js
@@ -0,0 +1,11 @@
+let word="the quick brown fox jumps overs the lazy dog"
+
+let list= word.split(" ")
+
+let finalword =""
+
+for(let i of list){
+    finalword+=i[0].toUpperCase()+i.slice(1,i.length)
+    finalword+=" "
+}
+console.log(finalword)
