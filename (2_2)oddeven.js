1c088d7b8db872cc14e943ee46e5735ee2344e2c
RAJKUMAR30082001 <125723189+RAJKUMAR30082001@users.noreply.github.com>
2023-12-04 11:11:14 +0530

    Add files via upload

---
+++ (2_2)oddeven.js
@@ -0,0 +1,10 @@
+let number=100
+findOddOrEven(number)
+findOddOrEven(71)
+function findOddOrEven(num){
+    if (num % 2 == 0){
+        console.log(`${num} is even`)
+    }
+    else
+    console.log(`${num} is odd`)
+}
---
+++ (3)percentage.js
@@ -0,0 +1,9 @@
+let marks=[80,40,90,70,50]
+let total=0
+for (let i=0;i<marks.length;i++){
+    total+=marks[i]
+}
+
+console.log(`the student total mark is ${total}`)
+console.log(`percentage of student is ${total/5}%`)
+
