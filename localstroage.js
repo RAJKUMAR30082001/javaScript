94950d2323cc8e3ab768d3838f1323124dea50f9
RAJKUMAR30082001 <125723189+RAJKUMAR30082001@users.noreply.github.com>
2023-12-06 16:39:14 +0530

    Add files via upload

---
+++ localstroage.js
@@ -0,0 +1,8 @@
+localStorage.setItem('names','rajkumar')
+localStorage.setItem('userid','2001')
+
+let name=localStorage.getItem(names)
+let id=localStorage.getItem(userid)
+console.log(`name:${names}`)
+console.log(`id:${id}`)
+
