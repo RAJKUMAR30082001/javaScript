3cb2d166365df593c6382b8ee8552d98ab8dc54e
RAJKUMAR30082001 <125723189+RAJKUMAR30082001@users.noreply.github.com>
2023-12-06 16:40:33 +0530

    Update and rename localstroage.js to 6-12-2001/localstroage.html

--- localstroage.js
+++
@@ -1,8 +0,0 @@
-localStorage.setItem('names','rajkumar')
-localStorage.setItem('userid','2001')
-
-let name=localStorage.getItem(names)
-let id=localStorage.getItem(userid)
-console.log(`name:${names}`)
-console.log(`id:${id}`)
-
